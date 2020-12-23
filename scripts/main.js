5b9b20e13cd76409568d957c2ea112cd7bc98b8a
Net <ibrotuber@gmail.com>
2020-12-22 22:55:04 -0300

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,39 @@
+let myHeading = document.querySelector('h1');
+myHeading.textContent = 'Pacman Fan Website';
+
+alert('Welcome to my first website!'); // alerta de bienvenida al cargar el sitio.
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function () {
+	let mySrc = myImage.getAttribute('src');
+	if (mySrc === 'images/622070_pacman.jpg') {
+		myImage.setAttribute('src', 'images/Pac-Man-0.png');
+		alert('Easter egg!');
+	} else {
+		myImage.setAttribute('src', 'images/622070_pacman.jpg');
+	}
+};
+
+let myButton = document.querySelector('button');
+//let myHeading = document.querySelector('h1')
+function setUserName() {
+	let myName = prompt('Please enter your name.');
+	if (!myName) {
+		setUserName();
+	} else {
+		localStorage.setItem('name', myName);
+		myHeading.innerHTML = 'Pacman is cool, ' + myName;
+	}
+}
+
+if (!localStorage.getItem('name')) {
+	setUserName();
+} else {
+	let storedName = localStorage.getItem('name');
+	myHeading.innerHTML = 'Pacman is cool, ' + storedName;
+}
+
+myButton.onclick = function () {
+	setUserName();
+};
